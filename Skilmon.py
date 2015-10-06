c97a18fc9383770423b38cadcaddee5ffb56cc27
Cole Moore <pokemario5@gmail.com>
2015-10-05 20:38:43 -0400

    save

---
+++ Skilmon.py
@@ -0,0 +1,31 @@
+
+
+class Move():
+    uid = 0
+    name = ""
+    description = ""
+    category = ""
+    learn_type = 
+    power = 
+    accuracy = 
+    pp = 
+    learned_at = 
+
+
+class ability():
+    uid = 0
+    name = 0
+    desription = 0 
+
+class Type():
+    uid = 0
+    name = 0
+    description = ""
+
+class Skilmon():
+    uid =
+    name = "mega blastoise"
+    description = ""
+    attack = 103
+    defense = 120
+
---
+++ skilmon/skilmon.py
@@ -0,0 +1,21 @@
+"""SkilMon"""
+
+class Skilmon():
+    uid = 0
+    exp = 0
+    happiness = 0
+    sp_attack = 0
+    hp = 0
+    sp_defense = 0
+    speed = 0 
+    total = 0
+    evolves_at = 0
+    species = ""
+    catch_rate = 0
+    growth_height = 0
+    height = 0
+    width = 0
+    mf_ratio = 0
+    abilties = []
+    moves = []
+    types = []
